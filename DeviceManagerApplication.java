758ee367ee7421983e54d4ff3e8f72c966e872a4
Waither Antony Karanja <tonnieantoh@gmail.com>
2025-07-18 13:26:17 +0300

    Create DeviceManagerApplication.java

---
+++ DeviceManagerApplication.java
@@ -0,0 +1,6 @@
+@SpringBootApplication
+public class DeviceManagerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DeviceManagerApplication.class, args);
+    }
+}
